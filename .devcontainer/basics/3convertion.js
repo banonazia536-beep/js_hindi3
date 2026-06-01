55068b7aa8cd04853c6928687de999fc347fe829
banonazia536-beep <banonazia536@gmail.com>
2026-06-01 04:02:30 +0000

    new

---
+++ .devcontainer/basics/3convertion.js
@@ -0,0 +1,56 @@
+/* 
+===================
+3} convertion 
+===================
+=> convertion in js is the process of changing a value from one datatype to another data type .
+
+type of convertion :-
+
+A] number to string:-
+*/
+console.log("number to string")
+let a = 12
+console.log(typeof a)
+console.log(a)
+console.log("after convertion")
+let newa =String (a)
+console.log(typeof newa)
+console.log(newa)
+
+//B] number to boolean:-
+console.log("number to boolean")
+let b = 2
+console.log(typeof b)
+console.log(b)
+console.log("after convertion")
+let newb = Boolean(b)
+console.log(typeof newb)
+console.log(newb)
+
+//C] string to number 
+console.log("string to number ")
+let c = "nazia"
+console.log(typeof c )
+console.log(c)
+console.log("after convertion")
+let newc = Number(c)
+console.log(typeof newc)
+console.log(newc)
+
+// D] string to boolean:-
+console.log("string to boolean")
+let d = "nazia"
+console.log(typeof d )
+console.log(d)
+console.log("after convertion")
+let newd = Boolean(d)
+console.log(typeof newd)
+console.log(newd)
+
+let e = ""
+console.log(typeof e )
+console.log(e)
+console.log("after convertion")
+let newe = Boolean(e)
+console.log(typeof newe)
+console.log(newe)
