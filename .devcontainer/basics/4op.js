ece8722dd111ce87aca5ebf002944e8aa1f85f92
banonazia536-beep <banonazia536@gmail.com>
2026-06-01 04:05:19 +0000

    new

---
+++ .devcontainer/basics/4op.js
@@ -0,0 +1,39 @@
+/* 
+=============
+4} operation :-
+=============
+=> those mathemetical operation which we use in programing lenguage 
+ */
+let a=2 ,b=3
+console.log(a+b)
+console.log(a-b)
+console.log(a*b)
+console.log(a/b)
+console.log(a%b)
+
+// more complex operations
+console.log(1+1)
+console.log("1"+2)
+console.log(1+"2")
+console.log("1"+"2")
+console.log("1"+2+2) // pehle string ka use karne se ye baki ko bhi boolean ki trah hi trit karega 
+console.log(1+2+"2")//bad me string ka use karne se strine se pehle ke code jo operation use kara hai vo perfome kar dega 
+
+//some boolean operations
+console.log(+true)
+console.log(+"")
+
+/*incriment or decriment operatiors 
+A. incriment operatior 
+   1.++a
+   2.a++
+B. decriment operatior 
+   1.--a
+   2.a--
+   */
+  let c=100
+  console.log(c++)// 1st print 2nd incriment
+  console.log(++c)//1st incriment 2nd print
+  console.log(--c)//1st decriment 2nd print 
+  console.log(c--)// 1st print 2nd dicriment
+  
