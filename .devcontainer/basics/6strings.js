395e9bcdb22a9468ee4e6de18f616d3ef3d71619
banonazia536-beep <banonazia536@gmail.com>
2026-06-01 04:37:35 +0000

    new

---
+++ .devcontainer/basics/6strings.js
@@ -0,0 +1,14 @@
+/*
+=============
+6} strings
+=============
+=> those data which we can write in "" .
+
+normal string */
+let a="12"
+console.log(a);
+//addition of stringa
+let b="nazia",c=" bano"
+console.log(b+c);
+
+// prient 
