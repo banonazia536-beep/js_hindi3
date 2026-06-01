385ef603fa428179edaf212d81334ee077de995d
banonazia536-beep <banonazia536@gmail.com>
2026-06-01 06:20:42 +0000

    new

--- .devcontainer/basics/6strings.js
+++ .devcontainer/basics/6strings.js
@@ -10,5 +10,9 @@ console.log(a);
 //addition of stringa
 let b="nazia",c=" bano"
 console.log(b+c);
+console.log(`name of user ${b}` );
 
 // prient 
+const gamename = new String('nazia')// in console it represent prototype 
+console.log(gamename); 
+
---
+++ .devcontainer/basics/test.js
@@ -0,0 +1,2 @@
+console.log("parteek");
+console.log("nazia");
